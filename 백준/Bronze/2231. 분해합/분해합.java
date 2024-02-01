382f138b9b9dc46c1a1ed63d4af0537097dd23bd
taeyeon5211 <128350472+taeyeon5211@users.noreply.github.com>
2024-02-01 18:45:38 +0900

    [Bronze II] Title: 분해합, Time: 220 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -5,26 +5,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-
-        int result = 0;
-
-        for (int i = 0; i < n; i++) {
-            int number = i;
-            int sum = 0;
-
-            while (number != 0) {
-                sum += number % 10;
-                number /= 10;
-            }
-
-            if (sum + i == n) {
-                result = i;
-                break;
-            }
-
-        }
-
-        System.out.println(result);
+        int N = scanner.nextInt();
+        findNum(N);
     }
+
+    static void findNum(int N) {
+        for (int i = 1; i < N; i++) {
+            if (getSumOfDigit(i) + i == N) {
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+
+    static int getSumOfDigit(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
 }

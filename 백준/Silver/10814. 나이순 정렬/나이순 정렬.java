81b13a0c1d82a191e2757080e31f4b9290ed2346
taeyeon5211 <128350472+taeyeon5211@users.noreply.github.com>
2024-02-28 19:09:26 +0900

    [Silver V] Title: 나이순 정렬, Time: 1888 ms, Memory: 320368 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -9,19 +9,13 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         String[] arr = new String[n];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = br.readLine() + " " + i;
+            arr[i] = br.readLine();
         }
 
         Arrays.sort(arr, (s1, s2) -> {
-            String[] split1 = s1.split(" ");
-            String[] split2 = s2.split(" ");
-            int age1 = Integer.parseInt(split1[0]);
-            int age2 = Integer.parseInt(split2[0]);
-            if (age1 != age2) {
-                return Integer.compare(age1, age2);
-            } else {
-                return Integer.compare(Integer.parseInt(split1[2]), Integer.parseInt(split2[2]));
-            }
+            int age1 = Integer.parseInt(s1.split(" ")[0]);
+            int age2 = Integer.parseInt(s2.split(" ")[0]);
+            return age1 - age2;
         });
 
         for (String s : arr) {

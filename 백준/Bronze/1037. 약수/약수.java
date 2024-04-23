37484b95a71e235ad299151f5c8097f6f9c99377
taeyeon5211 <128350472+taeyeon5211@users.noreply.github.com>
2024-04-24 01:34:14 +0900

    [Bronze I] Title: 약수, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = getMax(arr);
+        int min = getMin(arr, max);
+        System.out.println(max * min);
+    }
+
+    private static int getMax(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    private static int getMin(int[] arr, int max) {
+        int min = max;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}

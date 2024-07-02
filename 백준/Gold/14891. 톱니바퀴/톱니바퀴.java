7422af52a69de02bc83e86e7a224fbd6f28eeff9
taeyeon5211 <128350472+taeyeon5211@users.noreply.github.com>
2024-07-02 16:01:31 +0900

    [Gold V] Title: 톱니바퀴, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,47 +1,69 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class Main {
-    static int K;
-    static LinkedList<Character>[] wheel = new LinkedList[4];
-    static boolean[] chk;
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static LinkedList<String>[] q = new LinkedList[4];
+    static boolean[] visited;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for(int i = 0; i < 4; i++)
-            wheel[i] = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
 
-        for(int i = 0; i < 4; i++) {
-            String tmp = sc.next();
-            for(int j = 0; j < 8; j++)
-                wheel[i].add(tmp.charAt(j));
+        for (int i = 0; i < 4; i++) {
+            q[i] = new LinkedList<>();
+            fill(q[i]);
         }
 
-        K = sc.nextInt();
-        for(int i = 0; i < K; i++) {
-            chk = new boolean[4];
-            rotate(sc.nextInt() - 1, sc.nextInt());
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            String[] arr = s.split(" ");
+            visited = new boolean[4];
+            rotate(Integer.parseInt(arr[0]) - 1, Integer.parseInt(arr[1]));
         }
 
-        int ret = 0;
-        for(int i = 0; i < 4; i++)
-            if(wheel[i].get(0) == '1')
-                ret += Math.pow(2, i);
-
-        System.out.println(ret);
+        int result = 0;
+        for (int i = 0; i < 4; i++) {
+            if (q[i].get(0).equals("1")) {
+                result += Math.pow(2, i);
+            }
+        }
+        System.out.println(result);
     }
 
-    private static void rotate(int n, int d) {
-        if(n < 0 || n >= 4 || chk[n]) return;
-        chk[n] = true;
-        char left = wheel[n].get(6), right = wheel[n].get(2);
-        if(d == 1)
-            wheel[n].addFirst(wheel[n].pollLast());
-        else
-            wheel[n].addLast(wheel[n].pollFirst());
+    private static void rotate(int idx, int dir) {
 
-        if(n != 3 && right != wheel[n + 1].get(6))
-            rotate(n + 1, -d);
-        if(n != 0 && left != wheel[n - 1].get(2))
-            rotate(n - 1, -d);
+        if (idx < 0 || idx >= 4 || visited[idx]) {
+            return;
+        }
+
+        visited[idx] = true;
+        String right = q[idx].get(2);
+        String left = q[idx].get(6);
+
+
+        if (dir == 1) {
+            q[idx].addFirst(q[idx].pollLast());
+        } else {
+            q[idx].addLast(q[idx].pollFirst());
+        }
+
+        if (idx != 3 && !right.equals(q[idx + 1].get(6))) {
+            rotate(idx + 1, -dir);
+        }
+        if (idx != 0 && !left.equals(q[idx - 1].get(2))) {
+            rotate(idx - 1, -dir);
+        }
+    }
+
+    private static void fill(LinkedList<String> q) throws IOException {
+        String s = br.readLine();
+
+        for (int j = 0; j < s.length(); j++) {
+            q.add(String.valueOf(s.charAt(j)));
+        }
     }
 }

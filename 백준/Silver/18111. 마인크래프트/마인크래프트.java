05f9c4944fae88328d7bca1b1bed02b642f73000
taeyeon5211 <jty84373786@g.hongik.ac.kr>
2024-07-26 15:47:05 +0900

    [Silver II] Title: 마인크래프트, Time: 720 ms, Memory: 36520 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -9,70 +9,48 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        final int MAX_IVEN_SiZE = 64000000;
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N * M];
+        int[][] arr = new int[N][M];
 
         int max = -1;
-        int min = 257;
-
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                arr[i * M + j] = Integer.parseInt(st.nextToken());
-
-                max = Math.max(max, arr[i * M + j]);
-                min = Math.min(min, arr[i * M + j]);
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, arr[i][j]);
+                min = Math.min(min, arr[i][j]);
             }
         }
 
-        int maxHeight =  -1;
-        int minTime = 64000001 * 2;
+        int minSecond = Integer.MAX_VALUE;
+        int highBlock = 0;
 
-        for (int i = max; i >= min; i--) {
-            boolean isFail = false;
-            int time = 0;
-            int invenCnt = B;
+        for (int i = min; i <= max; i++) {
+            int second = 0;
+            int block = B;
 
-            for (int j = 0; j < N * M; j++) {
-
-                if (i < arr[j]) {
-                    int add = arr[j] - i;
-
-                    if (invenCnt > MAX_IVEN_SiZE) {
-                        isFail = true;
-                        break;
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (i > arr[j][k]) {
+                        second += i - arr[j][k];
+                        block -= i - arr[j][k];
+                    } else if (i < arr[j][k]) {
+                        second += (arr[j][k] - i) * 2;
+                        block += arr[j][k] - i;
                     }
-
-                    invenCnt += add;
-                    time += add * 2;
-                }
-
-            }
-
-            for (int j = 0; j < N * M; j++) {
-                
-                if (i > arr[j]) {
-                    int sub = i - arr[j];
-
-                    if (sub > invenCnt) {
-                        isFail = true;
-                        break;
-                    }
-
-                    invenCnt -= sub;
-                    time += sub;
                 }
             }
 
-            if (!isFail && time < minTime) {
-                maxHeight = i;
-                minTime = time;
+            if (block < 0) continue;
+            if (second <= minSecond) {
+                minSecond = second;
+                highBlock = Math.max(i, highBlock);
             }
         }
 
-        System.out.println(minTime + " " + maxHeight);
+        System.out.println(minSecond + " " + highBlock);
     }
 }

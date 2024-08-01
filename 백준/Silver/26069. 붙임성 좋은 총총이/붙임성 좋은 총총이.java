4214c5e99e76e242f0ec343f206cda0f762b6c22
taeyeon5211 <jty84373786@g.hongik.ac.kr>
2024-08-01 20:48:24 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 140 ms, Memory: 14796 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -2,43 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        String c = "ChongChong";
+        set.add(c);
 
         int N = Integer.parseInt(st.nextToken());
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String name1 = st.nextToken();
             String name2 = st.nextToken();
-            if (!map.containsKey(name1)) {
-                map.put(name1, 0);
-            }
-            if (!map.containsKey(name2)) {
-                map.put(name2, 0);
-            }
-            String c = "ChongChong";
-            if (name1.equals(c) || name2.equals(c)) {
-                map.put(name1, 1);
-                map.put(name2, 1);
-            } else {
-                if (map.get(name1) == 1 || map.get(name2) == 1) {
-                    map.put(name1, 1);
-                    map.put(name2, 1);
-                }
+
+            if (set.contains(name1) || set.contains(name2)) {
+                set.add(name1);
+                set.add(name2);
             }
         }
 
-        int result = 0;
-        for (Integer value : map.values()) {
-            result += value;
-        }
-        System.out.println(result);
+        System.out.println(set.size());
     }
 }

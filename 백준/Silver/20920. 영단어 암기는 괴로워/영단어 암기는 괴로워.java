c07837b9ad65036c0b85be7b1bbedc8e1aac8ba0
taeyeon5211 <jty84373786@g.hongik.ac.kr>
2024-08-02 11:39:37 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 776 ms, Memory: 38044 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0;i < N;i++) {
+            String s = br.readLine();
+            if (s.length() >= M) {
+                map.put(s, map.getOrDefault(s, 0) + 1);
+            }
+        }
+
+        List<String> words = new ArrayList<>(map.keySet());
+        Collections.sort(words, new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                if (map.get(o2) != map.get(o1)) {
+                    return Integer.compare(map.get(o2), map.get(o1));
+                }
+                if (o1.length() != o2.length()) {
+                    return o2.length() - o1.length();
+                }
+                return o1.compareTo(o2);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String word : words) {
+            sb.append(word).append("\n");
+        }
+        System.out.println(sb);
+    }
+}

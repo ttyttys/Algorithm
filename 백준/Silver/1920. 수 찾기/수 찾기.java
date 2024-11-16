13db1286cd1c2030b220c9d10b0d2b907bad55c0
taeyeon5211 <jty84373786@gmail.com>
2024-11-17 00:29:14 +0900

    [Silver IV] Title: 수 찾기, Time: 604 ms, Memory: 43036 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,31 +1,54 @@
 import java.io.*;
-import java.util.Collections;
-import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static TreeSet<String> ts = new TreeSet<>();
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        String[] a = br.readLine().split(" ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        int m = Integer.parseInt(br.readLine());
-        String[] b = br.readLine().split(" ");
+        int N = Integer.parseInt(br.readLine());
+        int[] arr1 = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
 
-        findNum(a, b);
-    }
-    static void findNum(String[] a, String[] b) throws IOException {
-        Collections.addAll(ts, a);
+        int M = Integer.parseInt(br.readLine());
+        int[] arr2 = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
 
-        for (String s : b) {
-            if (ts.contains(s)) {
+        Arrays.sort(arr1);
+
+        for (int i = 0; i < arr2.length; i++) {
+            if (binarySearch(arr1, arr2[i]) >= 0) {
                 bw.write("1\n");
             } else {
                 bw.write("0\n");
             }
         }
         bw.flush();
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }

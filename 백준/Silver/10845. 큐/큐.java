691fe0a16bb1f255316fcc2bf93a6a869f81c425
taeyeon5211 <jty84373786@gmail.com>
2024-12-18 20:44:38 +0900

    [Silver IV] Title: 큐, Time: 368 ms, Memory: 35144 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,57 +1,49 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
         int n = sc.nextInt();
-        int[] queue = new int[n];
+        int[] arr = new int[n];
         int begin = 0;
         int end = 0;
 
         while (n-- > 0) {
-            String s = sc.next();
-            switch (s) {
-                case "push" -> {
-                    int n1 = Integer.parseInt(sc.next());
-                    queue[end++] = n1;
+            String cmd = sc.next();
+
+            if (cmd.equals("push")) {
+                int num = sc.nextInt();
+                arr[end++] = num;
+            } else if (cmd.equals("pop")) {
+                if (begin == end) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(arr[begin++]).append("\n");
                 }
-                case "pop" -> {
-                    if (begin == end) {
-                        bw.write("-1\n");
-                    } else {
-                        bw.write(queue[begin] + "\n");
-                        queue[begin++] = 0;
-                    }
+            } else if (cmd.equals("size")) {
+                sb.append(end - begin).append("\n");
+            } else if (cmd.equals("empty")) {
+                if (begin == end) {
+                    sb.append("1\n");
+                } else {
+                    sb.append("0\n");
                 }
-                case "size" -> bw.write(end - begin + "\n");
-                case "empty" -> {
-                    if (begin == end) {
-                        bw.write("1\n");
-                    } else {
-                        bw.write("0\n");
-                    }
+            } else if (cmd.equals("front")) {
+                if (begin == end) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(arr[begin]).append("\n");
                 }
-                case "front" -> {
-                    if (begin == end) {
-                        bw.write("-1\n");
-                    } else {
-                        bw.write(queue[begin] + "\n");
-                    }
-                }
-                case "back" -> {
-                    if (begin == end) {
-                        bw.write("-1\n");
-                    } else {
-                        bw.write(queue[end - 1] + "\n");
-                    }
+            } else if (cmd.equals("back")) {
+                if (begin == end) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(arr[end - 1]).append("\n");
                 }
             }
         }
-        bw.flush();
+        System.out.println(sb);
     }
 }

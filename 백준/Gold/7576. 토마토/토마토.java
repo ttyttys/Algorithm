5879685d6ed1291f2f0f51e2390d0dc531148ff3
taeyeon5211 <jty84373786@gmail.com>
2024-12-28 16:55:17 +0900

    [Gold V] Title: 토마토, Time: 580 ms, Memory: 119912 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,21 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int M;
-    static int N;
+    static int N, M;
     static int[][] tomatoes;
     static int result = 0;
-
-    static Queue<Pair> queue = new LinkedList<>();
+    static Queue<Pair> q = new LinkedList<>();
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
     static class Pair {
-        int x;
-        int y;
-        int count;
+        int x, y, count;
 
         Pair(int x, int y, int count) {
             this.x = x;
@@ -24,27 +23,27 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        M = sc.nextInt();
-        N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         tomatoes = new int[N][M];
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                tomatoes[i][j] = sc.nextInt();
+                tomatoes[i][j] = Integer.parseInt(st.nextToken());
                 if (tomatoes[i][j] == 1) {
-                    queue.add(new Pair(i, j, 0));
+                    q.add(new Pair(i, j, 0));
                 }
             }
         }
-
         solve();
     }
 
-    static void bfs() {
-        while (!queue.isEmpty()) {
-            Pair p = queue.poll();
+    public static void bfs() {
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
             for (int i = 0; i < 4; i++) {
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
@@ -53,14 +52,15 @@ public class Main {
                 if (tomatoes[nx][ny] == 1) continue;
                 if (tomatoes[nx][ny] == -1) continue;
                 tomatoes[nx][ny] = 1;
-                queue.add(new Pair(nx, ny, p.count + 1));
+                q.add(new Pair(nx, ny, p.count + 1));
                 result = p.count + 1;
             }
         }
+
     }
 
-    static void solve() {
-        if (queue.size() == N * M) {
+    public static void solve() {
+        if (q.size() == N * M) {
             System.out.println(0);
             return;
         }

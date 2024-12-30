3a86e744df05f1702924207bc620fe802195e679
taeyeon5211 <jty84373786@gmail.com>
2024-12-30 23:55:13 +0900

    [Gold IV] Title: 테트로미노, Time: 652 ms, Memory: 35452 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                visited[i][j] = true;
+                dfs(i, j, 1, arr[i][j]);
+                visited[i][j] = false;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static void dfs(int x, int y, int cnt, int sum) {
+        if (cnt == 4) {
+            answer = Math.max(answer, sum);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (visited[nx][ny]) continue;
+
+            visited[nx][ny] = true;
+            if (cnt == 2) dfs(x, y, cnt + 1, sum + arr[nx][ny]);
+            dfs(nx, ny, cnt + 1, sum + arr[nx][ny]);
+            visited[nx][ny] = false;
+        }
+    }
+}
